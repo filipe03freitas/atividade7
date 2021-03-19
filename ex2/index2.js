2d12aab6d3eaa0a3a871b6431abde61989e9bef9
Filipe Freitas <filipe13freitas@gmail.com>
2021-03-19 11:25:45 -0300

    reparos de ortografia

--- ex2/index2.js
+++ ex2/index2.js
@@ -1,6 +1,5 @@
 // Pedindo os dados
 
-const nome = prompt('Informe seu nome: ');
 const pesoStr = prompt('Informe o seu peso: (Ex: 70.4)');
 const alturaStr = prompt('Informe sua altura: (Ex: 1.75)');
 
@@ -18,23 +17,23 @@ let imc = peso / (altura * altura);
 // Apresentar a resposta
 
 if (imc < 17) {
-    console.log(`O IMC e: (${imc.toFixed(2)}) esta muito abaixo do peso`);
+    console.log(`O IMC é: (${imc.toFixed(2)}) esta muito abaixo do peso`);
 
 } else if(imc >= 17 && imc < 18.5){
-    console.log(`O IMC e: (${imc.toFixed(2)}) esta abaixo do peso`);
+    console.log(`O IMC é: (${imc.toFixed(2)}) esta abaixo do peso`);
 
 } else if(imc >= 18.5 && imc < 25){
-    console.log(`O IMC e: (${imc.toFixed(2)}) Peso normal`);
+    console.log(`O IMC é: (${imc.toFixed(2)}) Peso normal`);
 
 } else if(imc >= 25 && imc < 30){
-    console.log(`O IMC e: (${imc.toFixed(2)}) Acima do peso`);
+    console.log(`O IMC é: (${imc.toFixed(2)}) Acima do peso`);
 
 } else if(imc >= 30 && imc < 35){
-    console.log(`O IMC e: (${imc.toFixed(2)}) Obesidade I`);
+    console.log(`O IMC é: (${imc.toFixed(2)}) Obesidade I`);
 
 } else if(imc >= 35 && imc < 40){
-    console.log(`O IMC e: (${imc.toFixed(2)}) Obesidade II`);
+    console.log(`O IMC é: (${imc.toFixed(2)}) Obesidade II`);
 
 } else{
-    console.log(`O IMC e: (${imc.toFixed(2)}) Obesidade III (mórbida)`)
+    console.log(`O IMC é: (${imc.toFixed(2)}) Obesidade III (mórbida)`)
 }
